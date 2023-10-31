66f54bd82dac9544fc00fa8b9a45f0c4e611afec
edu_milanez <edu.milanez@usp.br>
2023-10-31 09:40:56 -0300

    add: dynamic routing

--- codedare/codedare_app/urls.py
+++ codedare/codedare_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('posts', views.posts, name='posts')
+    path('posts', views.posts, name='posts'),
+    path('posts/<int:post_id>/', views.detail_post, name='detail')
 ]
--- codedare/codedare_app/views.py
+++ codedare/codedare_app/views.py
@@ -12,3 +12,9 @@ def index(request):
 def posts(request):
     context = {}
     return render(request, 'codedare_app/posts.html', context)
+
+def detail_post(request, post_id):
+    context = {
+        "post_id": post_id
+        }
+    return render(request, 'codedare_app/detail.html', context)
